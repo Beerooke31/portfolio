e37f53ca67159425e610e675458db68d3b17e2e9
Brooke <133279077+Beerooke31@users.noreply.github.com>
2025-04-26 18:17:42 +0800

    Changed About Me CSS and added Google Sheets to collect contact form data

--- Portfolio.js
+++ Portfolio.js
@@ -25,3 +25,21 @@ email.addEventListener("input", (event) => {
     email.setCustomValidity("");
   }
 });
+
+//Submitting form data to Google Sheets
+
+window.addEventListener("load", function () {
+  const form = document.getElementById("contact-form");
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: "POST",
+      body: data,
+    }).then(() => {
+      alert("Success!");
+      window.location.replace("/");
+    });
+  });
+});
